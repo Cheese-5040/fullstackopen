a330b6b5499e68fb8b6977f6329472e7c0f15b26
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-03 12:41:16 +0800

    event handler as a function

--- part1/1.0_intro/src/App.js
+++ part1/1.0_intro/src/App.js
@@ -6,19 +6,19 @@ const App = () => {
   //usestate returns array with 2 items, then we assign them as two variables, counter, adn setcounter
   //counter is set to 0, and setCounter is assigned to function to modify state
 
-  const handleClick = () => {
-    console.log("clicked");
-  }
+  //   const handleClick = () => {
+  //     console.log("clicked");
+  //   }
+  const increaseByOne = ()=>{setCounter(counter +1)}
+  const reset =()=>setCounter(0)
   return (
-      <div>
-          <div>
-              {counter}
-          </div>
-          <button onClick = {handleClick}>
-              plus
-          </button>
-      </div>
-  )
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      {/* event handler fucntion onClick */}
+      <button onClick={reset}>reset to 0</button>
+    </div>
+  );
 };
 
 export default App;
