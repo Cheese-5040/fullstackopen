ebb961c8cce9fcc183dcfd77b8e247a21041be7e
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-03 12:07:00 +0800

    parse object to anonymous functions

--- part1/1.0_intro/src/App.js
+++ part1/1.0_intro/src/App.js
@@ -1,28 +1,39 @@
 import React from 'react'
 
 const Hello = (order) => { //define hello function
-  //parse the object props and show its name 
+  //declaration of variables
+  const name = order.name
+  const age = order.age
+  //helper function to return year born of onjects
+  const createdTime = ()=>{
+    const currentTime = new Date().getFullYear()
+    return currentTime - age
+  }
+
+  //same helper function but use arrow function
+  const bornYear = () => { return new Date().getFullYear() - age}
+  const bornYear2 = () => new Date().getFullYear() - age //same as above
+
+
+
   return (//all react component usually need one root element (div)
     <div> 
-      <p> pelita mamak order :  {order.name}, created time is {order.time}</p>
+      <p> Hello {name} you are {age} years old </p>
+      <p> So you are probably born at {bornYear()} or {bornYear2()} or {createdTime()}</p>
     </div>
   )
 }
 
 
 const App = () => { //arrow fucntion assigned to const variable App
-  const createdTime = ()=>{
-    const currentTime = new Date().getFullDay()
-    return createdTime = props.time 
-  }
+  
   const name = "bom"
   const time = 10 
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name = "roti telur" time = {2+4} />
-      <Hello name = {name} time = {time} />
-      <p> So {name} were made in {createdTime}</p>
+      <Hello name = "roti telur" age = {2+4} />
+      <Hello name = {name} age = {time} />
     </div>
   )
 }
