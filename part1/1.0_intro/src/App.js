f9ef15ed8ac30796d40f29c1f76bc6a678f0e646
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-05 00:00:49 +0800

    conditional rendering

--- part1/1.0_intro/src/App.js
+++ part1/1.0_intro/src/App.js
@@ -1,26 +1,48 @@
 import React, { useState } from "react"; //import useState function
 
+const History = (props) =>{ //conditional rendering
+    if (props.allClicks.length === 0){ //if the length is 0 then return intro tag
+        return(
+            <div>the app is used by pressing the buttons</div>
+        )
+    }
+    return (
+        <div>button press history : {props.allClicks.join(' ')}</div>
+    )
+}
+
+const Button = ({handleClick, text}) => {
+    return( //remember to either use return or round brackets
+        <button onClick={handleClick}>
+        {text}
+        </button>
+    )
+}
+
 const App = () => {
     //initialize left state and setLeft function to set the state of left
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
     const [allClicks, setAll] = useState([])//array of states
+
+
     
-    const [clicks, setClicks] = useState({left: 0, right: 0})
-  
     const handleLeftClick = ()=> {
-        setClicks({...clicks, left: clicks.left + 1})
+        setAll(allClicks.concat('L')) //store click in all clicks array as "L"
+        setLeft(left + 1)
     }
 
     const handleRightClick = () => {
-        setClicks({...clicks, right: clicks.right +1})
+        setAll(allClicks.concat("R"))//store click in all clicks array as "L"
+        setRight(right +1)
     }
     return (
       <div>
-        {clicks.left}
-        <button onClick= {handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {left}
+        <Button handleClick= {handleLeftClick} text = 'left'/>
+        <Button handleClick={handleRightClick} text ='right'/>
+        {right}
+        <History allClicks = {allClicks}/> <p>History of all clicks</p>
       </div>
     )
   }
