d8c38d4880097d8bf1e0b110fa3b9669619dab6a
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-05 23:35:17 +0800

    event handling with hooks

---
+++ part1/1.0_intro/src/Appleftright.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react"; //import useState function
+
+const History = (props) =>{ //conditional rendering
+    if (props.allClicks.length === 0){ //if the length is 0 then return intro tag
+        return(
+            <div>the app is used by pressing the buttons</div>
+        )
+    }
+    return (
+        <div>button press history : {props.allClicks.join(' ')}</div>
+    )
+}
+
+const Button = ({handleClick, text}) => {
+    console.log(props)
+    const {onClick, text} = props
+    return( //remember to either use return or round brackets
+        <button onClick={handleClick}>
+        {text}
+        </button>
+    )
+}
+
+const App = () => {
+    //initialize left state and setLeft function to set the state of left
+    const [left, setLeft] = useState(0) // usestate must not be called from inside a loop, a conditional expression or anyplace that is not a funciton of defining a component
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])//array of states
+
+
+    
+    const handleLeftClick = ()=> {
+        setAll(allClicks.concat('L')) //store click in all clicks array as "L"
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat("R"))//store click in all clicks array as "L"
+        setRight(right +1)
+    }
+    return (
+      <div>
+        {left}
+        <Button handleClick= {handleLeftClick} text = 'left'/>
+        <Button handleClick={handleRightClick} text ='right'/>
+        {right}
+        <History allClicks = {allClicks}/> <p>History of all clicks</p>
+      </div>
+    )
+  }
+export default App;
