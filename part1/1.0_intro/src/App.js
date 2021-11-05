5da517b276944334210e6aab1e386c81c1c904c0
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-05 23:58:55 +0800

    shorthand for anonymous func in func

--- part1/1.0_intro/src/App.js
+++ part1/1.0_intro/src/App.js
@@ -4,22 +4,22 @@ import React, { useState } from "react"; //import useState function
 const App =()=>{
     const [value, setValue] = useState(10)
 
-    const hello =(who)=>{
-        const handleClick=()=>{
-            console.log("clicked the ", who)
-        }
-        return handleClick
+    const hello =(who)=>()=>{ //return funciton in function, same as return ()=>console.log()
+        console.log('hello',who)
     }
-    
+    const setToValue = (newValue) => () => {
+        setValue(newValue)
+    }
     
     return(
         <div>
             {value}
             {/* need to use arrow function since we need onClick to call a function and only reference to arrow function is set to event handler */}
             <button onClick={()=>setValue(0)}>reset to zero</button>
-            <button onClick = {hello("world")}>button</button>
-            <button onClick = {hello("Madam")}>button</button>
-            <button onClick = {hello("babi")}>button</button>
+            {/* buttons get their individualised event handlers using function in function  */}
+            <button onClick = {setToValue(1000)}>thousand</button>
+            <button onClick = {setToValue(0)}>reset</button>
+            <button onClick = {setToValue(value+1)}>increment</button>
 
         </div>
     )
