37f845cd9755e5cd4033cb042ef1c03a7ea418d0
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-05 23:55:12 +0800

    function in function

--- part1/1.0_intro/src/App.js
+++ part1/1.0_intro/src/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react"; //import useState function
 
+
 const App =()=>{
     const [value, setValue] = useState(10)
+
+    const hello =(who)=>{
+        const handleClick=()=>{
+            console.log("clicked the ", who)
+        }
+        return handleClick
+    }
+    
+    
     return(
         <div>
             {value}
-            <button>reset to zero</button>
+            {/* need to use arrow function since we need onClick to call a function and only reference to arrow function is set to event handler */}
+            <button onClick={()=>setValue(0)}>reset to zero</button>
+            <button onClick = {hello("world")}>button</button>
+            <button onClick = {hello("Madam")}>button</button>
+            <button onClick = {hello("babi")}>button</button>
+
         </div>
     )
 }
