069d8d903906f5fd71521e7ea0b69c91b29106b6
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-07 23:27:21 +0800

    set good issue

--- part1/1.6_unicafe/src/App.js
+++ part1/1.6_unicafe/src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+const Display = props => <div> {props.value}</div>
+
+//define button component 
+const Button = (props) =>(
+  <button onClick= {props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const setGoodToValue = (newValue) =>{ //setToValue function , take in newValue
+    setGood(newValue);
+    console.log(good)
+  }
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button onClick= {()=>setGoodToValue(good+1)} text = "good" />
+      <Display value = {good}/>
     </div>
   )
 }
