6d9f1f56fd6ff0e0fbdd56a3b382724495fcdd9f
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-13 23:41:53 +0800

    event handlers

--- part2/2.0_intro/src/App.js
+++ part2/2.0_intro/src/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react"; //import useState function
+import React from 'react'
 
+const App = (props) => {
+  const { notes } = props
 
-const App=() =>{
-  props = 10
-  console.log("props value is " + props)
   return (
-    <div >
-      test
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
