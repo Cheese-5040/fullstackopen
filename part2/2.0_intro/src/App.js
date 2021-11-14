7826f949d1f45f863dac414312510ec29dd067de
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-14 23:11:26 +0800

    accessing array using .map

--- part2/2.0_intro/src/App.js
+++ part2/2.0_intro/src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 const App = (props) => {
   const { notes } = props
-
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         <li>{notes[0].content}</li>
+        {/* accessing notes directly through index */}
         <li>{notes[1].content}</li>
         <li>{notes[2].content}</li>
+      </ul>
+      <ul>
+        this works as well
+        {notes.map(note => <li>{note.content}</li>)}
       </ul>
     </div>
   )
