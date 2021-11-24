82cbadbde21208a66589f06a9f334f163231c08b
Ow Yong Chee Seng <owyongcheeseng@gmail.com>
2021-11-24 22:40:25 +0800

    finding course

--- part2/2.1_course_info/src/App.js
+++ part2/2.1_course_info/src/App.js
@@ -1,5 +1,46 @@
-import React from 'react'
+import React from 'react';
 
+
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  )
+}
+
+const Course = ({course}) =>{
+  console.log(course)
+  return(
+    <>
+    <Header course = {course}/>
+    </>
+  )
+}
 const App = () => {
   const course = {
     id: 1,
@@ -23,7 +64,11 @@ const App = () => {
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <>
+      {course.map((note)=> <Course course={course.id} course = {course} />)}
+     
+    </>
+  )
 }
-
 export default App;
